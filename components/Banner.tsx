0c544aea7963ca53fcf52388e7245444f2a75e16
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2025-03-31 05:08:51 +0700

    style: cập nhật hình trong banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,18 +1,26 @@
-import Image from "next/image";
+import Image, { ImageProps } from 'next/image';
+import { cn } from '@/lib/utils';
 
-const Banner = ({ src, alt }) => {
-  return (
-    <div className="w-full h-auto">
-      <Image 
-        src={src} 
-        alt={alt} 
-        width={1920} 
-        height={600} 
-        className="w-full object-cover" 
-        priority
-      />
-    </div>
-  );
+interface BannerProps extends Omit<ImageProps, 'src' | 'alt'> {
+    src: string;
+    alt: string;
+    className?: string;
+}
+
+const Banner = ({ src, alt, className = '', ...imageProps }: BannerProps) => {
+    return (
+        <div className="w-full h-auto">
+            <Image
+                src={src}
+                alt={alt}
+                width={1920}
+                height={600}
+                className={cn('w-full object-cover', className)} 
+                priority
+                {...imageProps}
+            />
+        </div>
+    );
 };
 
 export default Banner;
